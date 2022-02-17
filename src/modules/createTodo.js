d60280708aff831875b4e0d32aa2e5a736b56045
Fernando <luisfernandosalasg@gmail.com>
2022-02-17 11:38:02 -0500

    Fix bug

--- src/modules/createTodo.js
+++ src/modules/createTodo.js
@@ -1,40 +1,45 @@
-import updateLocalStorage from './checkbox.js';
+import updateLocalStorage from "./checkbox.js";
 
 export default function createTodo(todos) {
-  const todosBody = document.querySelector('.todos-body');
-  todosBody.innerHTML = '';
+	const todosBody = document.querySelector(".todos-body");
+	todosBody.innerHTML = "";
 
-  const todoUl = document.createElement('ul');
+	const todoUl = document.createElement("ul");
 
-  todos.forEach((todo) => {
-    const li = document.createElement('li');
-    li.setAttribute('id', 'checkbox');
+	let index = 0;
 
-    if (todo.completed === true) {
-      li.className = 'line';
-    } else {
-      li.className = '';
-    }
+	todos.forEach((todo) => {
+		const li = document.createElement("li");
+		li.setAttribute("id", "checkbox");
 
-    const input = document.createElement('input');
-    input.id = todo.id;
-    input.type = 'checkbox';
-    input.name = 'checkbox';
+		if (todo.completed === true) {
+			li.className = "line";
+		} else {
+			li.className = "";
+		}
 
-    const spanEle = document.createElement('span');
-    spanEle.id = todo.id;
+		index += 1;
+		todo.id = index;
 
-    const btn = document.createElement('button');
-    input.checked = todo.completed;
-    li.appendChild(input);
-    spanEle.innerHTML = todo.description;
-    li.appendChild(spanEle);
-    li.appendChild(btn);
-    btn.id = todo.id;
-    btn.setAttribute('onclick', 'deleteTodo(this.id)');
-    btn.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    todoUl.appendChild(li);
-    todosBody.appendChild(todoUl);
-  });
-  updateLocalStorage(todos);
+		const input = document.createElement("input");
+		input.id = todo.id;
+		input.type = "checkbox";
+		input.name = "checkbox";
+
+		const spanEle = document.createElement("span");
+		spanEle.id = todo.id;
+
+		const btn = document.createElement("button");
+		input.checked = todo.completed;
+		li.appendChild(input);
+		spanEle.innerHTML = todo.description;
+		li.appendChild(spanEle);
+		li.appendChild(btn);
+		btn.id = todo.id;
+		btn.setAttribute("onclick", "deleteTodo(this.id)");
+		btn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+		todoUl.appendChild(li);
+		todosBody.appendChild(todoUl);
+	});
+	updateLocalStorage(todos);
 }
