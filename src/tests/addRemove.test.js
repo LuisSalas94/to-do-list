368a9c1f0b79f5b8e479503a4d51e52cc85e0f2d
Fernando <luisfernandosalasg@gmail.com>
2022-02-24 14:20:08 -0500

    add tests

--- src/tests/addRemove.test.js
+++ src/tests/addRemove.test.js
@@ -1,49 +1,26 @@
 /**
  * @jest-environment jsdom
  */
-import {jest} from '@jest/globals'
 import TodosClass from '../modules/todosClass.js';
-import getTasksLocalStorage from '../__mocks__/getLocalStorage.js';
-import setTasksLocalStorage from '../__mocks__/setLocalStorage.js';
-
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = String(value);
-  }
-}
 
 describe('Testing Add', () => {
-  
   it('New task description inserted in task container is abc', () => {
     const todos = new TodosClass();
     const tasksContainer = [{ description: 'abc', completed: true, index: 0 }];
-    
     const result = todos.create(tasksContainer[0]);
-    
     expect(result.description).toBe('abc');
   });
-
   it(' after saving a task local storage is length 1', () => {
     const tasksContainer = [{ description: 'abc', completed: true, index: 0 }];
-
-    setTasksLocalStorage(tasksContainer);
-    const result = getTasksLocalStorage();
-
+    localStorage.setItem('tasksContainer', JSON.stringify(tasksContainer));
+    const result = localStorage.getItem('tasksContainer')
+      ? JSON.parse(localStorage.getItem('tasksContainer'))
+      : [];
     expect(result.length).toBe(1);
   });
 });
-
 describe('Testing remove', () => {
   it('taskContainer length after deleting the only task, is 0', () => {
-
     document.body.innerHTML = ` <ul class="todos-list flex-container">
         <div class="flex-item" data-value="0">
         <div class="flex-container">
@@ -55,7 +32,6 @@ describe('Testing remove', () => {
         </div>
         </div>
         </ul>`;
-
     const removeTask = document.querySelector('.flex-item');
     const todos = new TodosClass();
     const tasksContainer = [
@@ -65,12 +41,8 @@ describe('Testing remove', () => {
         index: 0,
       },
     ];
-
     todos.create(tasksContainer[0]);
     todos.deleteTask(removeTask);
-
     expect(todos.list.length).toBe(0);
   });
 });
-
-global.localStorage = new LocalStorageMock();
